7b1eae04ed46fb22656ace253ea87e92937b0629
Glenn Poston <glenn.poston@inin.com>
2015-02-04 10:40:38 -0500

    move base_ami_info.txt back to /home/ec2-user

--- packer.sh
+++ packer.sh
@@ -8,9 +8,8 @@ sudo chown ec2-user:ec2-user /etc/ansible/ansible.cfg
 sudo touch /home/ec2-user/ansible.log
 sudo chown ec2-user:ec2-user /home/ec2-user/ansible.log
 sudo chmod 666 /home/ec2-user/ansible.log
-sudo mkdir -p /var/inin
-sudo echo "source_repo=$GIT_URL" >> /var/inin/base_ami_info.txt
-sudo echo "commit_hash=$GIT_COMMIT" >> /var/inin/base_ami_info.txt
-sudo echo "build=$JOB_NAME" >> /var/inin/base_ami_info.txt
-sudo echo "build_number=$BUILD_NUMBER" >> /var/inin/base_ami_info.txt
-sudo echo "build_url=$BUILD_URL" >> /var/inin/base_ami_info.txt
+sudo echo "source_repo=$GIT_URL" >> /home/ec2-user/base_ami_info.txt
+sudo echo "commit_hash=$GIT_COMMIT" >> /home/ec2-user/base_ami_info.txt
+sudo echo "build=$JOB_NAME" >> /home/ec2-user/base_ami_info.txt
+sudo echo "build_number=$BUILD_NUMBER" >> /home/ec2-user/base_ami_info.txt
+sudo echo "build_url=$BUILD_URL" >> /home/ec2-user/base_ami_info.txt
