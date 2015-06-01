eabd9093b7a79b3370ec26cdc7879a358dae8f70
Anthony Roach <anthony.roach@inin.com>
2015-06-01 12:54:08 -0400

    DEVOPS-3541: Pass the api_key from the base to the service playbook in a more ansible way.

--- base/roles/newrelic-server-alerts/templates/server_alerts.sh
+++ base/roles/newrelic-server-alerts/templates/server_alerts.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# /etc/newrelic/api_key is written out by newrelic-configure
-API_KEY=`cat /etc/newrelic/api_key`
-
 # set the alert policy list to the active servers
 {% if newrelic_server_alert_policy is defined %}
-python /opt/newrelic_server_alerts/server_alerts.py --api-key $API_KEY --role {{ service_role }} --policy {{ newrelic_server_alert_policy }}
+python /opt/newrelic_server_alerts/server_alerts.py --api-key {{ newrelic_api_key }} --role {{ service_role }} --policy {{ newrelic_server_alert_policy }}
 {% else %}
-python /opt/newrelic_server_alerts/server_alerts.py --api-key $API_KEY --role {{ service_role }} --policy {{ service_role }}
+python /opt/newrelic_server_alerts/server_alerts.py --api-key {{ newrelic_api_key }} --role {{ service_role }} --policy {{ service_role }}
 {% endif %}
 
